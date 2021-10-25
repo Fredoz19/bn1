0fd8b76f88a2d907011a329afdf8292e5aa2a99a
Fredoz19 <88060663+Fredoz19@users.noreply.github.com>
2021-10-25 20:07:20 +0200

    Update startNodeBSC.sh

--- startNodeBSC.sh
+++ startNodeBSC.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "Starting BSC Full Node Service: $(date)"
-/home/bscfullnode/bsc/build/bin/geth --config /home/bscfullnode/bsc/config.toml --datadir /home/bscfullnode/bsc/node --pprof.addr 0.0.0.0 --metrics --cache 64000 --txlookuplimit 0 --pprof --http --http.addr 0.0.0.0 --http.corsdomain '*' --http.api eth,net,web3,txpool,debug --ws --ws.addr 0.0.0.0 --ws.origins '*' --ws.api eth,net,web3,txpool,debug --nousb  --snapshot=false
+/home/bscfullnode/bsc/build/bin/geth --config /home/bscfullnode/bsc/config.toml --datadir /home/bscfullnode/bsc/node --pprof.addr 0.0.0.0 --metrics --cache 64000 --txlookuplimit 0 --pprof --http --http.addr 0.0.0.0 --http.corsdomain '*' --http.api eth,net,web3,txpool,debug --ws --ws.addr 0.0.0.0 --ws.origins '*' --ws.api eth,net,web3,txpool,debug --syncmode snap --diffsync --nousb  --snapshot=false
